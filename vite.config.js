d629b0191393fc21a6c388e6927bfbbe4981791b
Attila Farago <attila.farago.hu@gmail.com>
2025-03-19 00:49:08 +0100

    feat: refactor vite.config.js to support dynamic server configuration based on command

--- vite.config.js
+++ vite.config.js
@@ -7,24 +7,28 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  root: path.resolve(__dirname, '.'),
-  server: {
-    hot: true,
-    https: true,
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, "server.key")),
-    //   cert: fs.readFileSync(path.resolve(__dirname, "server.cert")),
-    // },
-    port: 5178,
-  },
-  plugins: [
-    https(),
-    // nodePolyfills({
-    //   protocolImports: true,
-    // }),
-  ],
-  build: {
-    outDir: './dist',
-  },
-});
+export default defineConfig(({ command }) => {
+  const isDebug = command === 'serve'; // vite dev
+  return {
+    root: path.resolve(__dirname, '.'),
+    server: {
+      hot: true,
+      https: true,
+      // https: {
+      //   key: fs.readFileSync(path.resolve(__dirname, "server.key")),
+      //   cert: fs.readFileSync(path.resolve(__dirname, "server.cert")),
+      // },
+      port: 5178,
+    },
+    plugins: [
+      isDebug ? https() : null,
+      // nodePolyfills({
+      //   protocolImports: true,
+      // }),
+    ],
+    build: {
+      outDir: './dist',
+    },
+  }
+}
+);
